d3afcc6d1cce756b02e2ef7b796ffc6ef01e9d30
shivampipv <shivam301296@gmail.com>
2020-03-20 15:13:45 +0530

    initial commit

---
+++ predictor.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import pandas as pd 
+
+
+print("Data preprocessing")
+
+ds_train= pd.read_csv("Google_Stock_Price_Train.csv")
+ds_train.head()
+
